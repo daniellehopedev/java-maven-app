c76ae02ce61f5e62e4acb89fddefa8731cc72f05
Danielle H <62916241+daniellehopedev@users.noreply.github.com>
2023-01-12 19:32:52 -0500

    correcting dockerhub repo tag

--- script.groovy
+++ script.groovy
@@ -6,7 +6,7 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        sh 'docker build -t danielleh/myrepo:maven-app-2.0 .'
+        sh 'docker build -t danielleh/my-repo:maven-app-2.0 .'
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
         sh 'docker push danielleh/my-repo:maven-app-2.0'
     }
